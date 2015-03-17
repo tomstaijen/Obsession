f62fe713c79c7c06d429a4ede7d7e84b06f04261
Tom Staijen <t.staijen@gmail.com>
2015-03-17 07:25:23 +0100

    akka and lua stuff

---
+++ Source/Obsession.Internal.LuaEngine/NeoLuaEngine.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoonSharp.Interpreter;
+using Neo.IronLua;
+
+namespace Obsession.Internal.LuaEngine
+{
+
+    public interface IEngine
+    {
+        void Register(string method, object callback);
+        object Run(string script);
+    }
+
+    public class MoonEngine : IEngine
+    {
+        private Script _script;
+
+        public MoonEngine()
+        {
+            _script = new Script();
+        }
+
+        public void Register(string method, object value)
+        {
+            _script.Globals[method] = value;
+        }
+
+        public object Run(string script)
+        {
+            return _script.DoString(script);
+        }
+
+        public dynamic Get(string fact)
+        {
+            return _script.Globals[fact];
+        }
+
+        public T Retrieve<T>(string expr)
+        {
+            var dynValue = _script.DoString(expr);
+        }
+    }
+
+    public class NeoLuaEngine
+    {
+        public void Test()
+        {
+            Lua lua = new Lua();
+            var g = lua.CreateEnvironment();
+            g.DefineMethod("Henkie", new Action(PrintHenkie));
+            g.DoChunk("Henkie()");
+        }
+
+        public void PrintHenkie()
+        {
+            Console.WriteLine("Henkie");
+        }
+    }
+}
---
+++ Source/Test/Engine/Lua/LuaTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Obsession.Internal.LuaEngine;
+
+namespace Test.Engine.Lua
+{
+    [TestFixture]
+    public class LuaTest
+    {
+        [Test]
+        public void MoonTest()
+        {
+            var engine = new MoonEngine();
+            engine.Register("name", "Pietje");
+            engine.Register("notify", (Action<string>)Notify);
+            Console.WriteLine(engine.Run(@"
+notify(name);
+return 1+2;
+            "));
+        }
+
+        [Test]
+        public void Test()
+        {
+            var lua = new NeoLuaEngine();
+            lua.Test();
+        }
+
+        public void Notify(string name)
+        {
+            Console.WriteLine("Hello {0}", name);
+        }
+    }
+}
